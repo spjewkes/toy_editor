6d620a7aae744a49276ed9dea4efe40191be943d
Steve Jewkes <steve@mulus.net>
2021-01-16 14:33:02 +0000

    Added editor rows

--- main.c
+++ main.c
@@ -85,15 +85,30 @@ char editorReadKey()
 /**
  * Output
  */
+
+void editorDrawRows()
+{
+	int y;
+	for (y = 0; y < 24; y++)
+	{
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
 void editorRefreshScreen()
 {
 	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
 	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /**
  * Input
  */
+
 void editorProcessKeypress()
 {
 	char c = editorReadKey();
