2052cf5b8f7883ce55955591d89c674b09e53cd8
Steve Jewkes <steve@mulus.net>
2021-01-17 11:19:26 +0000

    Added code to get screen size and cursor position

--- main.c
+++ main.c
@@ -88,15 +88,50 @@ char editorReadKey()
 	return c;
 }
 
+// Fetches the current position of the cursor
+int getCursorPosition(int *rows, int *cols)
+{
+	// Ask report the cursor position
+	if (!rows || !cols || write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+
+	// Store report in buffer
+	char buf[32];
+	unsigned int i = 0;
+	while (i < sizeof(buf) - 1)
+	{
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+		if (buf[i] == 'R')
+			break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	// Validate and try to fetch reported position
+	if (buf[0] != '\x1b' || buf[1] != '[')
+		return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return -1;
+
+	return 0;
+}
+
+// Get the size of the window
 int getWindowSize(int *rows, int *cols)
 {
-	struct winsize ws;
-
 	if (!rows || !cols)
 		return -1;
 
+	struct winsize ws;
 	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) == 12)
+		{
+			return getCursorPosition(rows, cols);
+		}
 		return -1;
+	}
 
 	*cols = ws.ws_col;
 	*rows = ws.ws_row;
